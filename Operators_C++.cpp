8f7b6b1c670cd3f0d34d8ecf6cb8c2fe568dc6cd
Poorrnima Vetrivelan <poorrnimavetrivelan@gmail.com>
2025-03-19 14:33:03 -0700

    Update Operators_C++.cpp

--- Operators_C++.cpp
+++ Operators_C++.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+using namespace std
+int main(){
+  //Unary - ++, --
+  int a=10,b,c;
+  b=a++; // B=A, A=A+1
+  c=++a; // A=A+1, C=A
 
+  int a1=10,b1,c1;
+  b1=a1--; 
+  c1=--a1; 
+
+  //Arithmetic
+  int d=2;
+  std::cout << a+d << endl << a-d << endl << a*d << endl << a%d << endl;
+
+  //Logical
+  // >
+  // <
+  // >=
+  // <==
+  // ==
+  // !=
+}
