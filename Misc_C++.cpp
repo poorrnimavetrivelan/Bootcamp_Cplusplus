ef9fd2285206b2013c968c6e52f9db3e402a8222
Poorrnima Vetrivelan <poorrnimavetrivelan@gmail.com>
2025-03-20 13:18:27 -0700

    Update and rename Jump_C++.cpp to Misc_C++.cpp

---
+++ Misc_C++.cpp
@@ -0,0 +1,41 @@
+// Break
+//Continue
+//Goto 
+
+#include <iostream>
+using namespace std;
+
+//Call by Value - Local Variable / Global Variable
+void swap1(int a, int b){
+    int temp = a;
+    a=b;
+    b=temp;
+    std::cout << "Inside Loop : A " << a << " B " << b << std::endl;
+}
+
+//Call by Reference
+void swappointer(int *a, int *b){
+    int temp = *a;
+    *a=*b;
+    *b=temp;
+    std::cout << "Inside Loop : A " << *a << " B " << *b << std::endl;
+}
+
+int main(){
+  int a=4, b=6;
+  
+  std::cout << "A " << a << " B " << b << std::endl;
+  swap1(a,b);
+  std::cout << "A " << a << " B " << b << std::endl;
+
+  swappointer(&a,&b);
+  std::cout << "A " << a << " B " << b << std::endl;
+
+  //Without using 3rd variable
+  a = a + b;
+  b = a - b;
+  a = a - b;
+  std::cout << "A " << a << " B " << b << std::endl;
+  
+  return 0;
+}
