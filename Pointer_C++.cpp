239c7c467436e35d903f130ea882e151eeb6b3ec
Poorrnima Vetrivelan <poorrnimavetrivelan@gmail.com>
2025-03-20 14:48:43 -0700

    Update Pointer_C++.cpp

--- Pointer_C++.cpp
+++ Pointer_C++.cpp
@@ -7,6 +7,7 @@ void swap ( int &c, int &d){
   c=d;
   d=temp;
 }
+
 int main(){
     int a=10;
     int &b=a;
@@ -39,6 +40,37 @@ int main(){
     int (*ptr)(int, int)=&add;
     result = ptr (20,30);
     std::cout << "Result is " << result << std::endl;
+    return 0;
+}
+
+//Dynamic Pointer
+#include <iostream>
+using namespace std;
+
+int main(){
+    int *ptr1;
+    int *ptr2;
+    int *ptr3;
+    int avg;
+    
+    ptr1=new int;
+    ptr2=new int;
+    ptr3=new int;
+    
+    std::cout << "Enter First number: " << std::endl;
+    cin >> *ptr1;
+    std::cout << "Enter Second number: " << std::endl;
+    cin >> *ptr2;
+    std::cout << "Enter Third number: " << std::endl;
+    cin >> *ptr3;
+    
+    avg = (*ptr1 + *ptr2 + *ptr3)/3;
+    
+    std::cout << "Average " << avg << std::endl;
+
+    delete ptr1;
+    delete ptr2;
+    delete ptr3;
     return 0;
 }
 
