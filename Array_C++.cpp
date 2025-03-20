360e83e4c3b20744172b8ec4ee1971cf9e94cbb0
Poorrnima Vetrivelan <poorrnimavetrivelan@gmail.com>
2025-03-20 13:27:16 -0700

    Update Array_C++.cpp

--- Array_C++.cpp
+++ Array_C++.cpp
@@ -1,1 +1,12 @@
 
+#include <iostream>
+using namespace std;
+
+int main(){
+    int ar1[5]={10,20,30,40,50};
+    int len_ar1=sizeof(ar1)/sizeof(ar1[0]);
+    for (int i=0 ; i<len_ar1 ; i++){
+        std::cout << ar1[i] << std::endl;
+    }
+    return 0;
+}
